093301f1ab4f4f5716af4f9e1c34190e398b83bc
Harry-Pekka Laakso <harry-pekka.laakso@knowit.fi>
2023-04-04 13:47:51 +0300

    Fix addon-a11y

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -17,26 +17,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  a11y: {
-    // Optional selector to inspect
-    element: '#root',
-    config: {
-      rules: [
-        {
-          // The autocomplete rule will not run based on the CSS selector provided
-          id: 'autocomplete-valid',
-          selector: '*:not([autocomplete="nope"])',
-        },
-        {
-          // Setting the enabled option to false will disable checks for this particular rule on all stories.
-          id: 'image-alt',
-          enabled: false,
-        },
-      ],
-    },
-    // Axe's options parameter
-    options: {},
-    // Optional flag to prevent the automatic check
-    manual: true,
-  },
 };
