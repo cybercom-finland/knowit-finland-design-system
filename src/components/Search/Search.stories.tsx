5dc8d17baa38c2d9c4449a173adf80f3bbc668a4
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-05-30 12:40:56 +0300

    add files

---
+++ src/components/Search/Search.stories.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import { withDesign } from 'storybook-addon-designs';
+import { Checkbox } from './Search';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Checkbox',
+  component: Checkbox,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    onChange: { action: true },
+  },
+  args: {
+    checked: false,
+    label: 'Label',
+    helperText: 'Helper text',
+    disabled: false,
+    indeterminate: false,
+    size: 'large',
+  },
+  parameters: {
+    design: [
+      {
+        name: 'light',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/%F0%9F%AA%81-Playground---IZ-Design-System?type=design&node-id=799%3A13731&t=aZ3vwdu5JAJD0sQm-1',
+      },
+      {
+        name: 'dark',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/%F0%9F%AA%81-Playground---IZ-Design-System?type=design&node-id=1533%3A85558&t=aZ3vwdu5JAJD0sQm-1',
+      },
+    ],
+  },
+  decorators: [withDesign],
+} as Meta<typeof Checkbox>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof Checkbox> = (args) => <Checkbox {...args} />;
+
+/**
+ * Default variant (not specified)
+ */
+export const DefaultVariant = Template.bind({});
+
+/**
+ * Disabled
+ */
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+/**
+ * Small checkbox
+ */
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+};
+/**
+ * With no helper text
+ */
+export const NoHelperText = Template.bind({});
+NoHelperText.args = {
+  helperText: undefined,
+};
---
+++ src/components/Search/Search.tsx
@@ -0,0 +1,236 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { variant } from 'styled-system';
+import {
+  spacing,
+  pxToRem,
+  typography,
+  Size,
+  generateRandomString,
+  InputComponentBaseProps,
+} from '../../shared';
+import { MdCheckBox } from 'react-icons/md';
+import { MdOutlineCheckBoxOutlineBlank } from 'react-icons/md';
+import { MdIndeterminateCheckBox } from 'react-icons/md';
+import { Label } from '../Label';
+import { HelperText } from '../HelperText';
+import { InputBaseProps } from '../Input';
+
+/**
+ * Various dimensions of checkbox component
+ */
+const checkboxDimensions = {
+  contentSpacing: spacing(1),
+  helperTextFontSize: pxToRem(14),
+  small: {
+    fontSize: pxToRem(16),
+    marginLeft: pxToRem(24),
+  },
+  large: {
+    fontSize: pxToRem(24),
+    marginLeft: pxToRem(32),
+  },
+};
+
+type CheckboxInputBaseProps = Omit<
+  InputBaseProps,
+  'error' | 'variant' | 'endIcon' | 'placeholder'
+>;
+
+/**
+ * Used HTML Attributes
+ */
+type CheckboxInputHTMLAttributes = Omit<
+  React.InputHTMLAttributes<HTMLInputElement>,
+  | 'size'
+  | 'type'
+  | 'accept'
+  | 'autoComplete'
+  | 'list'
+  | 'max'
+  | 'min'
+  | 'maxLength'
+  | 'minLength'
+  | 'multiple'
+  | 'pattern'
+  | 'placeholder'
+  | 'dirName'
+  | 'alt'
+  | 'capture'
+  | 'step'
+  | 'formAction'
+  | 'formEncType'
+  | 'formMethod'
+  | 'formNoValidate'
+  | 'formTarget'
+  | 'height'
+  | 'src'
+  | 'width'
+>;
+
+/**
+ * Checkbox component properties
+ * Extends html input element attributes
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/checkbox#additional_attributes
+ */
+export interface CheckboxProps
+  extends InputComponentBaseProps<HTMLInputElement>,
+    CheckboxInputBaseProps,
+    CheckboxInputHTMLAttributes {
+  /**
+   * Checkbox value
+   */
+  value?: React.InputHTMLAttributes<HTMLInputElement>['value'];
+
+  /**
+   * Checkbox is checked
+   */
+  checked?: boolean;
+
+  /**
+   * Checkbox is indeterminate state
+   */
+  indeterminate?: boolean;
+
+  /**
+   * Size of checkbox
+   */
+  size?: Exclude<Size, 'medium'>;
+}
+
+/**
+ * Helper function to calculate correct sizes for font size
+ * @param props mandatory checkbox props
+ * @returns modified css
+ */
+const calculateSizes = () => {
+  return css`
+    ${variant({
+      prop: 'size',
+      variants: {
+        small: {
+          fontSize: checkboxDimensions.small.fontSize,
+        },
+        large: {
+          fontSize: checkboxDimensions.large.fontSize,
+        },
+      },
+    })};
+  `;
+};
+
+/**
+ * calculate left margin for helper text
+ * @param props mandatory checkbox props
+ * @returns modified css
+ */
+const calculateMargin = (props: CheckboxProps) => {
+  return css`
+    margin-left: ${props.size == 'large'
+      ? checkboxDimensions.large.marginLeft
+      : checkboxDimensions.small.marginLeft} !important;
+  `;
+};
+
+const CheckboxWrapper = styled.span<CheckboxProps>`
+  ${calculateSizes}
+  color: ${(props) => props.theme.colors.grayScale.digitalBlack};
+  display: inline-flex;
+  font-family: ${typography.font};
+  font-weight: ${typography.weight.regular};
+  box-sizing: border-box;
+  align-items: center;
+  gap: ${checkboxDimensions.contentSpacing};
+  border: none;
+  cursor: pointer;
+
+  ${({ disabled }) =>
+    disabled &&
+    css`
+      color: ${(props) => props.theme.colors.grayScale.digitalBlack300};
+      cursor: default;
+    `}
+`;
+
+const CheckboxHelperText = styled(HelperText)`
+  ${calculateMargin}
+  font-size: ${checkboxDimensions.helperTextFontSize};
+  margin: 0;
+`;
+
+const CheckboxComponentWrapper = styled.span`
+  display: inline-flex;
+  flex-direction: column;
+`;
+
+const CheckboxLabel = styled(Label)`
+  margin: 0;
+`;
+
+/**
+ * Internal component styling
+ */
+const NativeCheckbox = styled.input`
+  display: none !important;
+`;
+
+/**
+ * Exported component
+ */
+export const Checkbox = ({
+  id,
+  label,
+  checked = false,
+  disabled = false,
+  indeterminate = false,
+  required = false,
+  size = 'large',
+  helperText,
+  ...restProps
+}: CheckboxProps) => {
+  const [boxChecked, setChecked] = React.useState(false);
+
+  // Sync checkbox to undelying input component
+  React.useEffect(() => {
+    setChecked(!!checked);
+  }, [checked]);
+
+  // Handle onclick, sync with underlying input component
+  const checkboxClicked = () => {
+    if (disabled) return;
+    checked = !boxChecked;
+    setChecked(!boxChecked);
+  };
+
+  // Use Id form props or create randomized string
+  const componentId = id ?? generateRandomString(5);
+
+  return (
+    <CheckboxComponentWrapper>
+      <CheckboxWrapper
+        id={componentId}
+        onClick={checkboxClicked}
+        size={size}
+        disabled={disabled}
+      >
+        {boxChecked && !indeterminate && <MdCheckBox />}
+        {!boxChecked && !indeterminate && <MdOutlineCheckBoxOutlineBlank />}
+        {indeterminate && <MdIndeterminateCheckBox />}
+        <NativeCheckbox
+          id={`checkbox-${componentId}`}
+          aria-labelledby={label && `label-${componentId}`}
+          aria-describedby={helperText && `helper-${componentId}`}
+          type={'checkbox'}
+          checked={boxChecked}
+          {...restProps}
+        />
+        <CheckboxLabel disabled={disabled} required={required}>
+          {label}
+        </CheckboxLabel>
+      </CheckboxWrapper>
+      <CheckboxHelperText size={size} disabled={disabled}>
+        {helperText}
+      </CheckboxHelperText>
+    </CheckboxComponentWrapper>
+  );
+};
