5b2b4e1ad2cd7c6f4a27b115239d77d472aaa2ce
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-06-02 13:34:24 +0300

    begin creating link component

---
+++ src/components/Link/Link.stories.tsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+
+import { Link } from './Link';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Link',
+  component: Link,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    children: { control: 'text' },
+  },
+  args: {},
+  parameters: {
+    design: [
+      {
+        name: 'light',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/%F0%9F%AA%81-Playground---IZ-Design-System?type=design&node-id=1732-24287&t=l6zGzl4XQt6kgMfa-4',
+      },
+      {
+        name: 'dark',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/%F0%9F%AA%81-Playground---IZ-Design-System?type=design&node-id=1737-24334&t=l6zGzl4XQt6kgMfa-4',
+      },
+    ],
+  },
+} as Meta<typeof Link>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof Link> = (args) => <Link {...args} />;
+
+/**
+ * Default
+ */
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {
+  children: 'Default label',
+};
---
+++ src/components/Link/Link.tsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import styled, { css } from 'styled-components';
+import { ComponentBaseProps, spacing } from '../../shared';
+import { MdOpenInNew } from 'react-icons/md';
+
+/**
+ * Link component properties
+ * Extends html link attributes
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#attributes
+ */
+export interface LinkProps
+  extends ComponentBaseProps<HTMLAnchorElement>,
+    React.AnchorHTMLAttributes<HTMLAnchorElement> {
+  /**
+   * Text content
+   */
+
+  children?: React.ReactNode;
+
+  /**
+   * Optional icon after the text
+   */
+  endIcon?: React.ReactNode;
+}
+
+/**
+ * Link inner component to wrap styles
+ */
+const InnerLink = styled.a<LinkProps>`
+  display: inline-flex;
+  text-decoration: underline;
+  gap: ${spacing(1)};
+  cursor: pointer;
+  &:hover {
+    color: ${(props) => props.theme.colors.info.info800};
+  }
+`;
+
+/**
+ * Link component
+ * @param props Link props
+ * @returns Link component
+ */
+export const Link = ({
+  endIcon = <MdOpenInNew />,
+  children,
+  ...restProps
+}: LinkProps) => {
+  return (
+    <InnerLink {...restProps}>
+      {children}
+      {endIcon}
+    </InnerLink>
+  );
+};
