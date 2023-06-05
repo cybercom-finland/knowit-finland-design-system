95f85fd5fb89ecb42a09ee32ffee35d5e28dc728
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-06-05 14:03:56 +0300

    remove unused imports

--- src/components/Link/Link.tsx
+++ src/components/Link/Link.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
+import styled from 'styled-components';
 import { ComponentBaseProps, spacing } from '../../shared';
-import { MdOpenInNew } from 'react-icons/md';
 
 /**
  * Link component properties
