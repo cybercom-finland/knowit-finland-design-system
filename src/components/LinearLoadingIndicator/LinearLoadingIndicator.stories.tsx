67ed329cb832797efa5a7827dedc7a77b341bd7c
Harry-Pekka Laakso <harry-pekka.laakso@knowit.fi>
2023-08-07 09:03:08 +0300

    Simplify DefaultTheme type

--- src/components/LinearLoadingIndicator/LinearLoadingIndicator.stories.tsx
+++ src/components/LinearLoadingIndicator/LinearLoadingIndicator.stories.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
 import { LinearLoadingIndicator } from './LinearLoadingIndicator';
-import { ComponentState } from '../../shared/types';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
--- src/styled.d.ts
+++ src/styled.d.ts
@@ -3,45 +3,6 @@ import 'styled-components';
 
 declare module 'styled-components' {
   export interface DefaultTheme {
-    colors: {
-      neutral: string;
-      grayScale: {
-        digitalBlack: string;
-        digitalBlack100: string;
-        digitalBlack200: string;
-        digitalBlack300: string;
-        digitalBlack400: string;
-        digitalBlack900: string;
-      };
-      additional: {
-        forest: string;
-        pear: string;
-        mint: string;
-        leather: string;
-        flamingo: string;
-        melon: string;
-        clay: string;
-      };
-      success: {
-        success: string;
-        success200: string;
-        success800: string;
-      };
-      warning: {
-        warning: string;
-        warning200: string;
-        warning800: string;
-      };
-      danger: {
-        danger: string;
-        danger200: string;
-        danger800: string;
-      };
-      info: {
-        info: string;
-        info200: string;
-        info800: string;
-      };
-    };
+    [key: string]: string;
   }
 }
