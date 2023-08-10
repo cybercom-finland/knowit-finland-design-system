2a832333fda5817f9f3145bbe9050ae93ef5e10b
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-08-10 13:40:12 +0300

    small fix

--- src/components/Dialog/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog/Dialog.tsx
@@ -2,8 +2,6 @@ import { HTMLProps } from 'react';
 
 export type DialogProps = Omit<HTMLProps<HTMLDivElement>, 'ref' | 'as'>;
 
-const Dialog = ({ children }: DialogProps) => {
+export const Dialog = ({ children }: DialogProps) => {
   return children;
 };
-
-export { Dialog };
