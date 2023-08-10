788aa510e72ab609955f7400f0975ecd1435f75a
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-08-10 13:19:06 +0300

    small fix

--- src/components/Dialog/DialogFooter/DialogFooter.tsx
+++ src/components/Dialog/DialogFooter/DialogFooter.tsx
@@ -6,18 +6,11 @@ import { DialogProps } from '../Dialog/Dialog';
 const NativeDialogFooter = styled.div`
   background: ${(props) => props.theme.colors.grayScale.digitalBlack100};
   padding: ${pxToRem(24)} ${pxToRem(32)} ${pxToRem(55)} ${pxToRem(16)};
-`;
-
-const Wrapper = styled.div`
   display: flex;
   justify-content: right;
   gap: ${pxToRem(20)};
 `;
 
 export const DialogFooter = ({ children }: DialogProps) => {
-  return (
-    <NativeDialogFooter>
-      <Wrapper>{children}</Wrapper>
-    </NativeDialogFooter>
-  );
+  return <NativeDialogFooter>{children}</NativeDialogFooter>;
 };
