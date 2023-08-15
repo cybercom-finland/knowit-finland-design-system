0bde69dac8d1ccf738ea507018206c2b1e6e8b1a
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-08-15 11:22:40 +0300

    fix

--- src/components/Dialog/Dialog/Dialog.tsx
+++ src/components/Dialog/Dialog/Dialog.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import { HTMLProps } from 'react';
 
-export type DialogProps = HTMLProps<HTMLDivElement>;
+/**
+ * Div component properties
+ * Extends html div element attributes
+ */
+export interface DialogProps extends HTMLProps<HTMLDivElement> {
+  /**
+   * Component children
+   */
+  children?: React.ReactNode;
+}
 
 export const Dialog = ({ children }: DialogProps) => {
   return <div role='dialog'>{children}</div>;
