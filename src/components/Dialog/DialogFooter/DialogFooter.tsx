ac03c754085533903fb64d4c6dbe30cd0e497666
Otto Lähepelto <7579254+olahepelto@users.noreply.github.com>
2023-08-16 12:02:43 +0300

    Feature/dialog (#44)
    add visual part of dialog

---
+++ src/components/Dialog/DialogFooter/DialogFooter.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { pxToRem } from '../../../shared';
+import styled from 'styled-components';
+import { DialogProps } from '../Dialog/Dialog';
+
+/**
+ * Dimensions of dialog header component
+ */
+const dialogHeaderDimensions = {
+  padding: `${pxToRem(24)} ${pxToRem(32)} ${pxToRem(55)} ${pxToRem(16)}`,
+  gap: pxToRem(20),
+  mobileBreakPoint: '375px',
+};
+
+const InnerDialogFooter = styled.div`
+  background: ${(props) => props.theme.colors.grayScale.digitalBlack100};
+  padding: ${dialogHeaderDimensions.padding};
+  display: flex;
+  justify-content: right;
+  gap: ${dialogHeaderDimensions.gap};
+  @media (max-width: ${dialogHeaderDimensions.mobileBreakPoint}) {
+    flex-direction: column;
+    align-items: end;
+  }
+`;
+
+export const DialogFooter = ({ children }: DialogProps) => {
+  return <InnerDialogFooter>{children}</InnerDialogFooter>;
+};
