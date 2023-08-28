e18e08d448120864317414f6f29dee6ae437d483
sebastiangustafsson <sebastian.gustafsson@knowit.fi>
2023-08-28 16:32:24 +0300

    tabs

--- src/components/Breadcrumb/Breadcrumb.stories.tsx
+++ src/components/Breadcrumb/Breadcrumb.stories.tsx
@@ -1,38 +1,47 @@
 import React from 'react';
 import { Meta, StoryFn } from '@storybook/react';
 import { withDesign } from 'storybook-addon-designs';
-import {Breadcrumb} from "./Breadcrumb";
-import {Link} from "../Link";
+import { Breadcrumb } from './Breadcrumb';
+import { Link } from '../Link';
+import { MdHome } from 'react-icons/md';
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
-    title: 'Components/Breadcrumbs',
-    component: Breadcrumb,
-    parameters: {
-        design: [
-            {
-                name: 'light',
-                type: 'figma',
-                url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=3174-14094&mode=design&t=PvrCSicTlqUTUDne-4',
-            },
-            {
-                name: 'dark',
-                type: 'figma',
-                url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=3217-16470&mode=design&t=PvrCSicTlqUTUDne-4',
-            },
-        ],
-    },
-    decorators: [withDesign],
+  title: 'Components/Breadcrumbs',
+  component: Breadcrumb,
+  parameters: {
+    design: [
+      {
+        name: 'light',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=3174-14094&mode=design&t=PvrCSicTlqUTUDne-4',
+      },
+      {
+        name: 'dark',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=3217-16470&mode=design&t=PvrCSicTlqUTUDne-4',
+      },
+    ],
+  },
+  decorators: [withDesign],
 } as Meta<typeof Breadcrumb>;
 
 // More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
 const Template: StoryFn<typeof Breadcrumb> = () => (
-    <Breadcrumb>
-        <Link>Home</Link>
-        <Link>Page</Link>
-        <Link>Page 2</Link>
-    </Breadcrumb>
+  <Breadcrumb
+    homeLink={
+      <Link>
+        {' '}
+        <MdHome />{' '}
+      </Link>
+    }
+  >
+    <Link>Link</Link>
+    <Link>Link</Link>
+    <Link>Link</Link>
+  </Breadcrumb>
 );
+
 /**
  * Default variant
  */
--- src/components/Breadcrumb/Breadcrumb.tsx
+++ src/components/Breadcrumb/Breadcrumb.tsx
@@ -1,21 +1,74 @@
-import React from 'react'
-import {ComponentBaseProps, generateRandomString} from "../../shared";
-import styled from "styled-components";
+import React from 'react';
+import {
+  ComponentBaseProps,
+  generateRandomString,
+  pxToRem,
+  typography,
+} from '../../shared';
+import styled from 'styled-components';
 
 export interface BreadcrumbProps extends ComponentBaseProps<HTMLDivElement> {
-    /**
-     * Children
-     */
-    children?: React.ReactNode,
+  /**
+   * Children
+   */
+  children?: React.ReactNode;
+
+  /**
+   * Home link
+   */
+  homeLink?: React.ReactNode;
 }
-const BreadcrumbWrapper = styled.div<BreadcrumbProps> `
-    display: flex;
-  flex-direction: row;
-`
-export const Breadcrumb = ({id, children}:BreadcrumbProps) => {
-    const componentId = id ?? generateRandomString(5);
+
+/**
+ * Wrapper for breadcrumb component
+ */
+const BreadcrumbWrapper = styled.div<BreadcrumbProps>`
+  display: inline-flex;
+  font-size: ${typography.size.paragraph2};
+`;
+
+/**
+ * Wrapper for breadcrumb spacer
+ */
+const BreadcrumbSpacer = styled.p<BreadcrumbProps>`
+  margin: 0 ${pxToRem(8)} 0 ${pxToRem(8)};
+`;
+
+/**
+ * Wrapper for breadcrumb home icon/link
+ */
+const HomeLinkWrapper = styled.p<BreadcrumbProps>`
+  margin: 0 ${pxToRem(8)} 0 0};
+`;
+
+/**
+ * Exported breadcrumbs component
+ */
+export const Breadcrumb = ({
+  id,
+  children,
+  homeLink,
+  ...restProps
+}: BreadcrumbProps) => {
+  const componentId = id ?? generateRandomString(5);
+  const renderChildren = React.Children.map(children, (child, index) => {
+    if (!React.isValidElement(child)) {
+      return child;
+    }
+    const elementChild: React.ReactElement = child;
     return (
-        <BreadcrumbWrapper id={componentId}>
-        {children}
-        </BreadcrumbWrapper>)
-}
+      <>
+        {elementChild}
+        {index < React.Children.count(children) - 1 && (
+          <BreadcrumbSpacer> / </BreadcrumbSpacer>
+        )}
+      </>
+    );
+  });
+  return (
+    <BreadcrumbWrapper id={componentId} {...restProps}>
+      {homeLink && <HomeLinkWrapper>{homeLink}</HomeLinkWrapper>}
+      {renderChildren}
+    </BreadcrumbWrapper>
+  );
+};
