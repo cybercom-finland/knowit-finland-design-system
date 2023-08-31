ef9b3a4c0082f2ce3b699ddcabe76ff71e826b3c
Otto Lähepelto <otto.lahepelto@gmail.com>
2023-08-31 13:36:21 +0300

    accordion

--- src/components/Accordion/Accordion.tsx
+++ src/components/Accordion/Accordion.tsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react';
-import styled, { css } from 'styled-components';
-import { variant } from 'styled-system';
+import styled from 'styled-components';
 import { pxToRem, ComponentBaseProps } from '../../shared';
 import { IconButton } from '../IconButton';
 import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
@@ -9,8 +8,17 @@ import { MdKeyboardArrowDown, MdKeyboardArrowUp } from 'react-icons/md';
  * Dimensions of accordion component
  */
 const accordionDimensions = {
-  rounded: {
-    borderRadius: pxToRem(32),
+  accordionTitle: {
+    paddingTop: 16,
+    paddingRigth: 16,
+    paddingBottom: 16,
+    paddingLeft: 24,
+  },
+  accordionContent: {
+    paddingTop: 0,
+    paddingRigth: 24,
+    paddingBottom: 24,
+    paddingLeft: 24,
   },
 };
 
@@ -30,27 +38,7 @@ export interface AccordionProps extends ComponentBaseProps<HTMLDivElement> {
   children: React.ReactNode;
 }
 
-/**
- * Helper function to calculate correct sizes
- * @param props mandatory accordion props
- * @returns modified css
- */
-const calculateSizes = () => {
-  return css`
-    ${variant({
-      prop: 'variant',
-      variants: {
-        rounded: {
-          overflow: 'hidden',
-          borderRadius: `0 ${accordionDimensions.rounded.borderRadius} 0 ${accordionDimensions.rounded.borderRadius}`,
-        },
-      },
-    })};
-  `;
-};
-
 const InternalAccordion = styled.div<AccordionProps>`
-  ${calculateSizes}
   background-color: ${(props) => props.theme.colors.grayScale.digitalBlack100};
 `;
 
@@ -59,10 +47,20 @@ const AccordionTitle = styled.div<AccordionProps>`
   flex-direction: row;
   justify-content: space-between;
   cursor: pointer;
-  padding: 16px 16px 16px 24px;
+  user-select: none;
+  padding: ${pxToRem(accordionDimensions.accordionTitle.paddingTop)}
+    ${pxToRem(accordionDimensions.accordionTitle.paddingRigth)}
+    ${pxToRem(accordionDimensions.accordionTitle.paddingBottom)}
+    ${pxToRem(accordionDimensions.accordionTitle.paddingLeft)};
 `;
 const AccordionContent = styled.div<AccordionProps>`
-  padding: 0px 24px 24px 24px;
+  padding: ${pxToRem(accordionDimensions.accordionContent.paddingTop)}
+    ${pxToRem(accordionDimensions.accordionContent.paddingRigth)}
+    ${pxToRem(accordionDimensions.accordionContent.paddingBottom)}
+    ${pxToRem(accordionDimensions.accordionContent.paddingLeft)};
+`;
+const AccordionArrow = styled.div<AccordionProps>`
+  align-self: center;
 `;
 
 /**
@@ -78,10 +76,10 @@ export const Accordion = ({
     <InternalAccordion {...restProps}>
       <AccordionTitle onClick={() => setAccordionOpen(!accordionIsOpen)}>
         {title}
-        <IconButton aria-label='toggle accordion'>
+        <AccordionArrow>
           {accordionIsOpen && <MdKeyboardArrowUp />}
           {!accordionIsOpen && <MdKeyboardArrowDown />}
-        </IconButton>
+        </AccordionArrow>
       </AccordionTitle>
       {accordionIsOpen && <AccordionContent>{children}</AccordionContent>}
     </InternalAccordion>
