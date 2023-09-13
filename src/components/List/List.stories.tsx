c8fe841afdf95ab6a1dc73b25a2212ef094b6d65
Kati Salin <kati.salin@knowit.fi>
2023-09-13 16:24:32 +0300

    Added the List and ListItem components

---
+++ src/components/List/List.stories.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { StoryFn, Meta } from '@storybook/react';
+import { List, ListItem } from './List';
+
+export default {
+  title: 'Components/List',
+  component: List,
+  parameters: {
+    design: [
+      {
+        name: 'light',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=2669-18946&mode=design&t=Ja0iChxOXQ0eT24I-4',
+      },
+      {
+        name: 'dark',
+        type: 'figma',
+        url: 'https://www.figma.com/file/qUvylGh5ubOWlpqlplVORt/IZ-Design-System---%F0%9F%9A%80-Live?type=design&node-id=2669-19704&mode=design&t=Ja0iChxOXQ0eT24I-4',
+      },
+    ],
+  },
+} as Meta<typeof List>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: StoryFn<typeof List> = (args) => (
+  <List>
+    <ListItem {...args} text='Berries' expandButton={true}>
+      <ListItem {...args} text='Strawberry' />
+      <ListItem {...args} text='Blueberry' />
+      <ListItem {...args} text='Blackberry' />
+    </ListItem>
+    <ListItem {...args} text='Fruits' expandButton={true}>
+      <ListItem {...args} text='Yellow' expandButton={true}>
+        <ListItem {...args} text='Banana' />
+        <ListItem {...args} text='Pineapple' />
+      </ListItem>
+      <ListItem {...args} text='Red' expandButton={true}>
+        <ListItem {...args} text='Apple' />
+        <ListItem {...args} text='Peach' />
+      </ListItem>
+    </ListItem>
+    <ListItem {...args} text='Potato' />
+  </List>
+);
+
+/**
+ * Default
+ */
+export const Default = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Default.args = {};
---
+++ src/components/List/List.tsx
@@ -0,0 +1,133 @@
+import React from 'react';
+import styled from 'styled-components';
+import { typography } from '../../shared';
+import { IconButton } from '../IconButton/IconButton';
+import { MdOutlineExpandLess, MdOutlineExpandMore } from 'react-icons/md';
+import { listItemPadding } from './styles';
+
+/**
+ * ListItem component properties
+ * Extends html label attributes
+ * https://developer.mozilla.org/en-US/docs/Web/HTML/Element/label#attributes
+ */
+export interface ListItemProps
+  extends React.LabelHTMLAttributes<HTMLLabelElement> {
+  /**
+   * Text of the list item
+   */
+  text?: string;
+
+  /**
+   * Does the item have a button for expanding sub items
+   */
+  expandButton?: boolean;
+
+  /**
+   * Aria-label for the expand button
+   */
+  expandButtonAriaLabel?: string;
+
+  /**
+   * Is the list item by default expanded (true) or unexpanded (false)
+   */
+  expanded?: boolean;
+}
+
+/**
+ * Style for the area which contains the list item's text
+ */
+const TextWrapper = styled.div`
+  font-size: ${typography.size.paragraph};
+  line-height: ${typography.lineHeight.paragraph};
+  flex: 1;
+  display: inline-block;
+  padding: ${listItemPadding};
+`;
+
+/**
+ * Style for the area which contains the list item's expand button
+ */
+const ExpandButtonWrapper = styled.div`
+  padding-right: ${listItemPadding};
+  flex: 1;
+  display: inline-block;
+`;
+
+/**
+ * Style for the list item div, which contains the text and the expand button
+ */
+const ListItemWrapper = styled.div`
+  display: flex;
+  padding: listItemSpacing;
+  align-items: flex-start;
+  justify-content: flex-start;
+  display: inline-block;
+  color: ${(props) => props.theme.colors.grayScale.digitalBlack};
+  &:hover {
+    color: ${(props) => props.theme.colors.grayScale.digitalBlack400};
+  }
+`;
+
+/**
+ * Style for the div that contains the sub items (creates indentation)
+ */
+const ListSubItemWrapper = styled.div`
+  padding-left: ${listItemPadding};
+`;
+
+/**
+ * List component's main div, containing all the ListItems
+ */
+export const ListWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+/**
+ * List component (contains ListItems)
+ */
+export const List = ({
+  children,
+}: React.LabelHTMLAttributes<HTMLLabelElement>) => {
+  return <ListWrapper>{children}</ListWrapper>;
+};
+
+/**
+ * ListItem component (may contain other ListItems hiearchially)
+ * @param props ListItem props
+ * @returns ListItem component
+ */
+export const ListItem = ({
+  text,
+  expandButton,
+  expandButtonAriaLabel,
+  children,
+  expanded,
+}: ListItemProps) => {
+  const [expandedState, setExpandedState] = React.useState(expanded);
+
+  return (
+    <div>
+      <ListItemWrapper>
+        <TextWrapper>{text}</TextWrapper>
+        {expandButton && (
+          <ExpandButtonWrapper>
+            <IconButton
+              onClick={() => {
+                setExpandedState(!expandedState);
+              }}
+              aria-label={expandButtonAriaLabel || 'List expand button'}
+            >
+              {expandedState ? (
+                <MdOutlineExpandLess />
+              ) : (
+                <MdOutlineExpandMore />
+              )}
+            </IconButton>
+          </ExpandButtonWrapper>
+        )}
+      </ListItemWrapper>
+      {expandedState && <ListSubItemWrapper>{children}</ListSubItemWrapper>}
+    </div>
+  );
+};
